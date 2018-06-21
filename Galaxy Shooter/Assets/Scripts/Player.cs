1ea05fedc2bf464aa95e626f88dcd01c54937fe9
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-06-22 00:26:51 +0530

    Set up the player
    The player snaps to the origin when the game starts.

---
+++ Galaxy Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	void Start ()
+	{
+		transform.position = new Vector3(0, 0, 0); //transform has a Vector3 position
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
