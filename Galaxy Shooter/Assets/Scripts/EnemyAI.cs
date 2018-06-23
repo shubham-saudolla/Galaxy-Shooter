d9295f17b318a0db8314a173c555ab382ad90e3c
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-06-23 17:36:25 +0530

    Implemented basic Enemy AI behaviour

---
+++ Galaxy Shooter/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+	private float _speed = 5.0f;
+	void Start ()
+	{
+		
+	}
+	
+	void Update () 
+	{
+		transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+		if(transform.position.y < -7.0f)
+		{
+			float randomX = Random.Range(-7f, 7f);
+			transform.position = new Vector3(randomX, 7, 0);
+		}
+	}
+}
