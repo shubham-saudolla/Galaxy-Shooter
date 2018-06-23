c4d9d7031652f9800ff7bd911d84f4dfec1727a3
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-06-23 10:56:04 +0530

    Updated triple shot
    The tripleShot now gets destroyed on colliding with the player

---
+++ Galaxy Shooter/Assets/Scripts/powerUp.cs
@@ -0,0 +1,34 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerUp : MonoBehaviour
+{
+	[SerializeField]
+	private float _speed = 3.0f;
+	
+	void Update () 
+	{
+		transform.Translate(Vector3.down * _speed * Time.deltaTime);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			Player player = other.GetComponent<Player>();
+
+			if(player != null)
+			{
+				player.canTripleShot = true;
+			}
+			
+			Destroy(this.gameObject);
+		}
+	}
+}
