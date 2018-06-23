0dfa32f4987236a663b42e706c08f1d69b523870
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-06-23 11:23:44 +0530

    Updated powerup script to handle all types of powerups

--- Galaxy Shooter/Assets/Scripts/powerUp.cs
+++ Galaxy Shooter/Assets/Scripts/powerUp.cs
@@ -11,7 +11,9 @@ public class powerUp : MonoBehaviour
 {
 	[SerializeField]
 	private float _speed = 3.0f;
+	[SerializeField]
 	
+	private int powerupID;
 	void Update () 
 	{
 		transform.Translate(Vector3.down * _speed * Time.deltaTime);
@@ -25,8 +27,24 @@ public class powerUp : MonoBehaviour
 
 			if(player != null)
 			{
-				player.TripleShotPowerupOn(); //when you destroy an object with the coroutine, the coroutine gets destroyed
+				if(powerupID == 0)
+				{
+					player.TripleShotPowerupOn(); //when you destroy an object with the coroutine, the coroutine gets destroyed
 				//thus we have used another function in player to call the coroutine and then destroyed the powerup
+				}
+				else if(powerupID == 1)
+				{
+					//enable speed boost
+				}
+				else if(powerupID == 2)
+				{
+					//enable shields
+				}
+				else
+				{
+					Debug.Log("Powerup with ID: " + powerupID + " not found")
+				}
+				
 			}
 			
 			Destroy(this.gameObject);
