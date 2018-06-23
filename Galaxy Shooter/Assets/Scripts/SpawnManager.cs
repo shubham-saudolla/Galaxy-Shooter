e149d506343143561d4595c5da523c31aba7756e
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-06-24 01:39:43 +0530

    Set up spawn manager and enemy spawning

---
+++ Galaxy Shooter/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,31 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject enemyShipPrefab;
+	[SerializeField]
+	private GameObject[] powerUps;
+
+	void Start ()
+	{
+		StartCoroutine(EnemySpawnRoutine());
+	}
+	
+	//coroutine to spawn an enemy every five seconds
+	IEnumerator EnemySpawnRoutine()
+	{
+		while(true)
+		{
+			Instantiate(enemyShipPrefab, new Vector3(Random.Range(-7f, 7f), 7, 0), Quaternion.identity);
+			yield return new WaitForSeconds(5.0f);
+		}
+	}
+}
