2ef3d0c0d6f3af9541c000a119c04dceb9600aca
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-06-25 23:19:51 +0530

    Set up player movement animations

---
+++ Galaxy Shooter/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,43 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+	private Animator _anim;
+
+	void Start ()
+	{
+		_anim = GetComponent<Animator>();
+	}
+	
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			_anim.SetBool("Turn_Left", true);
+			_anim.SetBool("Turn_Right", false);
+		}
+		else if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			_anim.SetBool("Turn_Left", false);
+			_anim.SetBool("Turn_Right", false);
+		}
+
+		if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			_anim.SetBool("Turn_Left", false);
+			_anim.SetBool("Turn_Right", true);
+		}
+		else if(Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+		{
+			_anim.SetBool("Turn_Left", false);
+			_anim.SetBool("Turn_Right", false);
+		}
+	}
+}
